04b47ea1744aaa1cc5dcc58506579912a1493881
Tom T <90579774+TTroczynski@users.noreply.github.com>
2023-10-07 11:20:32 -0400

    Adding tests for load_model and preprocess_img

--- test_FlaskTests.py
+++ test_FlaskTests.py
@@ -1,7 +1,16 @@
 import pytest
 import model
+import flask
+from PIL import Image
 
 def test_preprocess_img_given_IncorrectPath_FailsToProcess():
         with pytest.raises(Exception) as exceptionInfo:
                 model.preprocess_img(" ")
         assert str(exceptionInfo.value) != "";
+
+def test_load_Model_GivenNoPath_FailsToLoadModel():
+        with pytest.raises(Exception) as ExceptionInfo:
+                model.load_model(" ")
+        assert str(ExceptionInfo.value) != "";
+
+
